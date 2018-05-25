6173ba450108cbb497644b6e8c26ee93eb047145
Neil Mather <ngmather@gmail.com>
2018-05-25 14:55:40 +0100

    Update text

--- src/langs/en.js
+++ src/langs/en.js
@@ -1,18 +1,18 @@
 //Add strings
 gdprCookieNoticeLocales.en = {
-  description: 'We place essential cookes but would like to use cookies to offer you a better browsing experience, personalise content and ads, to provide social media features and to analyse our traffic. Please click <strong class="gdpr-cookie-notice-settings-button">Cookie Settings</strong> to enable additional cookings or Ok to continue with just essential cookies.',
+    description: 'We use cookies to help our website function, and analytical cookies to improve our website. Third party cookies are added by Soundcloud, Youtube and Stripe. Please click <strong class="gdpr-cookie-notice-settings-button">Cookie Settings</strong> to amend cookie settings or click OK to accept all.',
   settings: 'Cookie settings',
   accept: 'OK',
   revoke: 'Cookie settings',
-  statement: 'Our cookie statement',
+  statement: 'Our cookie policy',
   save: 'Save settings',
   always_on: 'Always on',
   cookie_essential_title: 'Essential website cookies',
   cookie_essential_desc: 'Necessary cookies help make a website usable by enabling basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.',
   cookie_performace_title: 'Performance cookies',
-  cookie_performace_desc: 'These cookies are used to enhance the performance and functionality of our websites but are non-essential to their use. For example it stores your preferred language or the region that you are in.',
+  cookie_performace_desc: 'These cookies are used to enhance the performance and functionality of our websites but are non-essential to their use. For example they are used by our hosting service for load balancing.',
   cookie_analytics_title: 'Analytics cookies',
-  cookie_analytics_desc: 'We use analytics cookies to help us measure how users interact with website content, which helps us customize our websites and application for you in order to enhance your experience.',
+    cookie_analytics_desc: 'We use analytical cookies to analyse the use of our website and its performance, in order to improve it (for example, to count the number of visitors and to see how visitors move around our website when they are using it).',
   cookie_marketing_title: 'Marketing cookies',
-  cookie_marketing_desc: 'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.'
+    cookie_marketing_desc: 'We do not use these directly, but some of the third-party services that we embed do use these type of cookies. These cookies record your activity as you use a service in order to target advertising to you.  3rd party opt-out information here: <a href="https://policies.google.com/technologies/cookies">YouTube</a>; <a href="https://soundcloud.com/pages/cookies">SoundCloud</a>; <a href="https://stripe.com/gb/privacy">Stripe</a>.'
 }
